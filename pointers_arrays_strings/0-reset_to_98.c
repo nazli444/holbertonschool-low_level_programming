863815a58e4b6151471f83edcce24c50842d908b
nazli444 <nazlinemetova@icloud.com>
2025-12-09 21:54:43 +0000

    Task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of an int to 98
- * @n: pointer to the int to update
+ * reset_to_98 - updates the value of the integer it points to to 98
+ * @n: pointer to the integer to update
+ *
+ * Return: void
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
