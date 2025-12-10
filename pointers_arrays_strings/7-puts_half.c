084c779b54a91522775f299515bb1e01bae9da9e
nazli444 <nazlinemetova01@icloud.com>
2025-12-10 19:44:55 +0000

    Task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,22 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    /* calculate the length of the string */
-    while (str[length] != '\0')
-        length++;
+	while (str[length] != '\0')
+		length++;
 
-    /* start index for second half */
-    if (length % 2 == 0)
-        i = length / 2;
-    else
-        i = (length + 1) / 2;
+	if (length % 2 == 0)
+		i = length / 2;
+	else
+		i = (length + 1) / 2;
 
-    /* print second half */
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
