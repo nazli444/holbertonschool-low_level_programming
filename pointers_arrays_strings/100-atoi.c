abfbd00f2f3830d4e9056063505dd7c626cdf4ad
nazli444 <nazlinemetova01@icloud.com>
2025-12-10 19:55:19 +0000

    Task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0;
+	int i = 0, sign = 1, num = 0, started = 0;
 
 	while (s[i] != '\0')
 	{
@@ -16,10 +16,11 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			started = 1;
 			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
 		}
+		else if (started)
+			break;
 		i++;
 	}
 
