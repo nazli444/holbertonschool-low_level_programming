08015ecf6316b9ca5084accea2c72cb749b6fd94
nazli444 <nazlinemetova01@icloud.com>
2025-12-10 20:38:40 +0000

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
