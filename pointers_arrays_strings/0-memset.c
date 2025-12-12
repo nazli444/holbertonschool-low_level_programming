ae12f80391838e7c353ed2e862d39f7911686270
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 20:12:17 +0000

    Task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of memory area s with constant byte b
+ * _memset - fills memory with a constant byte
  * @s: pointer to memory area
- * @b: constant byte to fill
+ * @b: the constant byte
  * @n: number of bytes to fill
  *
  * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        s[i] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-    return (s);
+	return (s);
 }
