31d58975cf78ce506dcdbdc2a4e4b8cb11a5087b
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 20:34:30 +0000

    Task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: bytes to match
+ *
+ * Return: number of bytes in initial segment of s consisting only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+				break;
+			a++;
+		}
+		if (!*a)
+			return (count);
+		count++;
+		s++;
+	}
+	return (count);
+}
