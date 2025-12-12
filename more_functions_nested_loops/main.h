7e49b6ab45e52f1107fbb7986f220126177230be
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 09:21:15 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_square(int size);
+/* Function prototypes for Holberton projects */
 
-#endif
+/* _putchar writes the character c to stdout */
+int _putchar(char c);
+
+/* Function prototypes for printing shapes */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* MAIN_H */
