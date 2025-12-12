e1e70391f7eee521ea3e96c6c388d713e18f27e7
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 20:07:50 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 void _putchar(char c);
 void puts2(char *str);
