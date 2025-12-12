6c043fee59d0863980ee8b49cd63c91838aa54bc
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 18:44:44 +0000

    Task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    /* find the end of dest */
-    while (dest[i] != '\0')
-        i++;
+	/* find the end of dest */
+	while (dest[i] != '\0')
+		i++;
 
-    /* copy src to the end of dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* copy src to the end of dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* add terminating null byte */
-    dest[i] = '\0';
+	/* add terminating null byte */
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
