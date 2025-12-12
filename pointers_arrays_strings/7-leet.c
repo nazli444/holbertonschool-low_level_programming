07ed6b3df08cbb546181fd126bb62ef7aa7abb9e
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 19:29:52 +0000

    main.h

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,21 +7,20 @@
  */
 char *leet(char *s)
 {
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; letters[j] != '\0'; j++)
-        {
-            if (s[i] == letters[j])
-            {
-                s[i] = numbers[j];
-                break;
-            }
-        }
-    }
-
-    return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
