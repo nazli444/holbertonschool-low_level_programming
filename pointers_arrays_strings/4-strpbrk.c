74dd8afe950abbc7c9caa296640353456877d11e
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 20:50:46 +0000

    Task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: string containing bytes to match
+ * Return: pointer to first matching byte in s, or NULL if none found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+
+	return (0);
+}
