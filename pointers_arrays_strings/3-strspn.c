c167bde0fecc50bed961326fff56f92adceb003c
nazli444 <nazlinemetova01@icloud.com>
2025-12-12 20:45:58 +0000

    Task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,30 @@
-while (*s)
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: string containing allowed characters
+ * Return: number of bytes in initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
 {
-	a = accept;
-	while (*a)
-	{
-		if (*s == *a)
-			break;
-		a++;
-	}
-	if (!*a)
-		return (count);
-	count++;
-	s++;
+    unsigned int count = 0;
+    char *a;
+
+    while (*s)
+    {
+        a = accept;
+        while (*a)
+        {
+            if (*s == *a)
+                break;
+            a++;
+        }
+        if (!*a)
+            return (count);
+        count++;
+        s++;
+    }
+
+    return (count);
 }
