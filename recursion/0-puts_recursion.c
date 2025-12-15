2dd50b0a768dcf78c5abdb256d5f1dc5f4dd457f
nazli444 <nazlinemetova01@icloud.com>
2025-12-15 09:02:55 +0000

    Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case: end of string */
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);    /* Print current character */
-    _puts_recursion(s + 1); /* Recursive call on the next character */
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
