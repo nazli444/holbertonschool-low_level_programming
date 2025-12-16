4482db37179aef3b46176d9c7230c518a97dbaff
nazli444 <nazlinemetova01@icloud.com>
2025-12-16 19:23:00 +0000

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: argument count
+ * @argv: argument vector (not used)
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /* argv is unused */
+	printf("%d\n", argc - 1);
+	return (0);
+}
