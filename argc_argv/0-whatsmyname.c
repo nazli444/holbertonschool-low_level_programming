58b3d270ee067c5d752acff50548d05b85f1a12b
nazli444 <nazlinemetova01@icloud.com>
2025-12-16 19:12:37 +0000

    Task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name, followed by a new line
+ * @argc: argument count (not used)
+ * @argv: argument vector (array of strings)
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc; // argc is unused, so we silence the warning
+    printf("%s\n", argv[0]);
+    return 0;
+}
