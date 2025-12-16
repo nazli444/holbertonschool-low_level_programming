5df7b1b91901eff5cb20e9956ed5f5e99942e390
nazli444 <nazlinemetova01@icloud.com>
2025-12-16 19:14:18 +0000

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argc; // argc is unused, so we silence the warning
+    (void)argc; /* argc is unused, so we silence the warning */
     printf("%s\n", argv[0]);
     return 0;
 }
