56423ddc16f6ba81da800b5f97cbe54d719a32c3
root <nazlinemetova01@icloud.com>
2025-12-22 20:23:41 +0000

    Task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#knkskxnbsmsbckanskbxms
+char *create_array(unsigned int  size, char c)
+{
+    char *arr;
+    unsigned int i;
+#jvj cnmn mmmkknbm
+    if (size==0)
 
+        return (NULL);
+    arr=malloc(sizeof(char)*size);
+    if (arr==NULL)
+        return (NULL);
+#hvjvhjnvjkjvn jhvkhv uhggioljv
+    for (i=0; i<size; i++)
+    arr[i]=c;
+#gb nbbnb kbn
+    return (arr);
+}
