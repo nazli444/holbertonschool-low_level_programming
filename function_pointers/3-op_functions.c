ba8bdcaf782e7cc937d46459153f7e6a23847dfb
Your Name <you@example.com>
2026-01-01 21:15:10 +0000

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,40 +1,57 @@
 #include "3-calc.h"
-
 /**
- * op_add - adds two numbers
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtracts two numbers
+ * op_sub - subtracts one integer from another
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of a minus b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiplies two numbers
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divides two numbers
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of a divided by b
  */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - modulo of two numbers
+ * op_mod - calculates the remainder of the division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided by b
  */
+
 int op_mod(int a, int b)
 {
 	return (a % b);
