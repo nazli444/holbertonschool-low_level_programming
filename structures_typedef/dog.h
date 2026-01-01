565951a00969dffe0e0520a4023ba4b90b53fbe8
Your Name <you@example.com>
2026-01-01 14:42:27 +0000

    Dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*fhfghb kgffbmbfuy bjnjb
+*hgh knknk knazl
+*kanaknnm kkl jf rt
+*hgfjv nm
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
