9a8b056516a4295d1dd0034fa9413611cf5939e4
Your Name <you@example.com>
2026-01-01 16:35:03 +0000

    Task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,13 +7,12 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    if (d->name != NULL)
-        free(d->name);
-    if (d->owner != NULL)
-        free(d->owner);
-    free(d);
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
 }
-
