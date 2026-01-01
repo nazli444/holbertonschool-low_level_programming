faad91611d0421241f83716f7e7045a700228e96
Your Name <you@example.com>
2026-01-01 14:55:48 +0000

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
+#include <stddef.h> /* NULL */
 #include "dog.h"
 
 /**
  * init_dog - initialize a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * name: name of the dog
+ * age: age of the dog
+ * owner: owner of the dog
  *
  * Description: This function sets the name, age, and owner
  * fields of the given struct dog.
