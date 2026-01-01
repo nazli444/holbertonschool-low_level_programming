0f32bd27f1be4e8df3d5b15cac9b0101c08a01ba
Your Name <you@example.com>
2026-01-01 14:45:20 +0000

    Dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-*fhfghb kgffbmbfuy bjnjb
-*hgh knknk knazl
-*kanaknnm kkl jf rt
-*hgfjv nm
-*/
+ * struct dog - a dog's basic info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner's name
+ *
+ * Description: This structure stores basic information
+ * about a dog, including its name, age, and owner.
+ */
 struct dog
 {
 	char *name;
