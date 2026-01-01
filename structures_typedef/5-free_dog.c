6947c0fefff2cd9fb3164fef1d4c4c4be6255a8a
Your Name <you@example.com>
2026-01-01 16:32:18 +0000

    Task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include "dog.h"   /* <- buraya əlavə et */
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * free_dog - frees a dog_t struct
+ * @d: pointer to dog to free
  */
-int main(void)
+void free_dog(dog_t *d)
 {
-	dog_t *my_dog;
+    if (d == NULL)
+        return;
 
-	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-
-	free_dog(my_dog);  /* free the memory */
-
-	return (0);
+    if (d->name != NULL)
+        free(d->name);
+    if (d->owner != NULL)
+        free(d->owner);
+    free(d);
 }
+
