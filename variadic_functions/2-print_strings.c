a14d4600ae7f961d12e877dfbc0ddcf4b38a7901
Your Name <you@example.com>
2026-01-10 17:32:40 +0000

    Task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_strings - prints strings followed by a new line
- * @separator: string to be printed between strings
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
  */
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -31,3 +31,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	printf("\n");
 }
+
