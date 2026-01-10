3af2b6755fc6763ef3629b5ee4529701c84232f7
Your Name <you@example.com>
2026-01-10 18:45:34 +0000

    Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_all - prints anything based on a format string
- * @format: string containing types of arguments
- *          c = char, i = integer, f = float, s = string
+ * print_all - prints anything based on format
+ * @format: types of arguments: c=char, i=int, f=float, s=string
  */
 void print_all(const char * const format, ...)
 {
@@ -17,24 +16,30 @@ void print_all(const char * const format, ...)
 
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'c')
-			printf("%s%c", sep, va_arg(args, int));
-		if (format[i] == 'i')
-			printf("%s%d", sep, va_arg(args, int));
-		if (format[i] == 'f')
-			printf("%s%f", sep, va_arg(args, double));
 		if (format[i] == 's')
 		{
 			str = va_arg(args, char *);
 			if (str == NULL)
 				str = "(nil)";
 			printf("%s%s", sep, str);
-		}
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
 			sep = ", ";
+		}
+
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f')
+		{
+			if (format[i] == 'c')
+				printf("%s%c", sep, va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%s%d", sep, va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
+		}
+
 		i++;
 	}
 
 	va_end(args);
 	printf("\n");
 }
+
