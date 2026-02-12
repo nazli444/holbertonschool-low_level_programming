cf73f0607e382a248760ff8438ce9931930224ff
Your Name <you@example.com>
2026-02-12 08:49:59 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,43 +4,51 @@
 #include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: name of the file
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters printed, 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r, w;
-    char *buffer;
+	int fd;
+	ssize_t r, w;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    r = read(fd, buffer, letters);
-    if (r == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    w = write(STDOUT_FILENO, buffer, r);
-    if (w != r)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w != r)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
-    return (w);
+	free(buffer);
+	close(fd);
+	return (w);
 }
+
