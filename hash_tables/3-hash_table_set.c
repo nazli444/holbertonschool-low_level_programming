d61cd77b6fb0c2f17d440eec99e1f438f4c24bd6
Your Name <you@example.com>
2026-08-13 10:09:07 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,41 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - creates a new hash node
+ * @key: the key
+ * @value: the value
+ *
+ * Return: new node or NULL
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+
+	return (node);
+}
+
+/**
  * hash_table_set - adds or updates an element in a hash table
  * @ht: the hash table
  * @key: the key
@@ -13,14 +48,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
 	hash_node_t *current;
+	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-
 	current = ht->array[index];
 
 	while (current != NULL)
@@ -36,24 +70,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
