d519194daf01b2a769ecb395457b7cbd3ef63b79
Your Name <you@example.com>
2026-08-13 10:02:29 +0000

    1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -24,7 +24,7 @@ unsigned long int hash_djb2(const unsigned char *str)
  *
  * Return: index of the key
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+
 {
 	return (hash_djb2(key) % size);
 }
