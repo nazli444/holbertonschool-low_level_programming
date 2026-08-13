8a70ae56b21d5b87aace3756d735f7793da89930
Your Name <you@example.com>
2026-08-13 10:13:21 +0000

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ *
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int first;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	first = 1;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			if (!first)
+				printf(", ");
+
+			printf("'%s': '%s'", current->key, current->value);
+			first = 0;
+			current = current->next;
+		}
+	}
+
+	printf("}\n");
+}
